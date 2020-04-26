9d25acbc87054ef83245c049a93cc4de3fdc51e8
DawsonFlannery <dawsonliu7@gmail.com>
2020-04-26 17:54:05 +0700

    Initial commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+	$host = "localhost";
+	$user = "root";
+	$pass = "";
+	$db = "monitoring_aktivitas_siswa";
+
+	$con = mysqli_connect($host, $user , $pass , $db) or die('KONEKSI GAGAL');
+  ?>
